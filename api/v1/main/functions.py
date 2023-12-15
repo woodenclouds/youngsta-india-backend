7a620123359012579dabaa440b4cccf1fe7d3754
Wazeer Ahmed <wazeer@hashinclude.io>
2023-12-15 15:53:36 +0530

    updated functions

--- api/v1/main/functions.py
+++ api/v1/main/functions.py
@@ -26,10 +26,10 @@ def generate_serializer_errors(args):
 
 class Permissions(permissions.BasePermission):
 
-    def is_plant(self, request, view):
+    def is_admin(self, request, view):
         user = request.user
-        is_plant = user.groups.filter(name='Plant')
-        return is_plant
+        admin = user.groups.filter(name='admin')
+        return admin
 
 
 def get_auto_id(model):
@@ -41,28 +41,6 @@ def get_auto_id(model):
     auto_id = model.objects.all().count()+1
     return auto_id
 
-
-# class GroupRequiredMixin(UserPassesTestMixin):
-#     group_required = None
-#     # print(UserPassesTestMixin,"------------------------------------------------------------------")
-
-#     def test_func(self):
-#         user = self.request.user
-#         if not user.is_authenticated:
-#             return False
-#         if self.group_required is None:
-#             return True
-#         if isinstance(self.group_required, str):
-#             groups = [self.group_required]
-#             print(groups,"====================================================")
-#         elif isinstance(self.group_required, (list, tuple)):
-#             groups = self.group_required
-#         else:
-#             raise ValueError("Invalid group_required attribute")
-#         return user.groups.filter(name__in=groups).exists()
-
-#     # def handle_no_permission(self):
-#     #     raise PermissionDenied()
 
 def get_first_letters(string):
     words = string.split()
@@ -76,31 +54,31 @@ def get_first_letters(string):
 
 
 
-def send_notification(token,title,message,device_type):
-    # token = "ffBIYT4tzUREh7-NputjPh:APA91bFAjm5y_jUg1e_kDDZYfOiXhsc_suEWNmNV8CZDCwK1L2aoEB4LY0mhDIun2K8C43-0fhk5HIXHGDviPOgthAs8WkyJO_a0Q-MdmZQLYARI0cRT-oVtkNJVaOuLAIGpCEuDyl-Z"
-    print(settings.FIREBASE_CREDENTIALS)
-    if not firebase_admin._apps:
-        credentials = firebase_admin.credentials.Certificate(settings.FIREBASE_CREDENTIALS)
-        firebase_admin.initialize_app(credentials,options={'projectId': 'jeeva-77175'})
-        firebase_admin.get_app()
-    else:
-         firebase_admin.get_app()
-    multicast_message = messaging.MulticastMessage(
-    tokens=[token],
-    notification=messaging.Notification(title, message) if device_type.lower() == "android" else None,
-        data={
-            "title": title,
-            "body": message,
-            "type": "DATA",
-        },
-        apns=messaging.APNSConfig(
-            payload=messaging.APNSPayload(
-                aps=messaging.Aps(
-                    content_available=1
-                )
-            )
-        )
-    )
-    message_res = messaging.send_multicast(
-            multicast_message)
-    return message_res
+# def send_notification(token,title,message,device_type):
+#     # token = "ffBIYT4tzUREh7-NputjPh:APA91bFAjm5y_jUg1e_kDDZYfOiXhsc_suEWNmNV8CZDCwK1L2aoEB4LY0mhDIun2K8C43-0fhk5HIXHGDviPOgthAs8WkyJO_a0Q-MdmZQLYARI0cRT-oVtkNJVaOuLAIGpCEuDyl-Z"
+#     print(settings.FIREBASE_CREDENTIALS)
+#     if not firebase_admin._apps:
+#         credentials = firebase_admin.credentials.Certificate(settings.FIREBASE_CREDENTIALS)
+#         firebase_admin.initialize_app(credentials,options={'projectId': 'jeeva-77175'})
+#         firebase_admin.get_app()
+#     else:
+#          firebase_admin.get_app()
+#     multicast_message = messaging.MulticastMessage(
+#     tokens=[token],
+#     notification=messaging.Notification(title, message) if device_type.lower() == "android" else None,
+#         data={
+#             "title": title,
+#             "body": message,
+#             "type": "DATA",
+#         },
+#         apns=messaging.APNSConfig(
+#             payload=messaging.APNSPayload(
+#                 aps=messaging.Aps(
+#                     content_available=1
+#                 )
+#             )
+#         )
+#     )
+#     message_res = messaging.send_multicast(
+#             multicast_message)
+#     return message_res
